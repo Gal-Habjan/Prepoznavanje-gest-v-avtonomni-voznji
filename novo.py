6261d09de55ae4db09e305c52caff38ad5323cf6
eva <eva.vizjak@student.um.si>
2024-05-30 21:53:33 +0200

    Update novo.py
    Dodan batch normalization pa drop pred dense layerjem.

--- novo.py
+++ novo.py
@@ -90,25 +90,32 @@ from tensorflow.keras import layers, models
 #     layers.Input(shape=(X_train.shape[1], X_train.shape[2], 1)),
 #     layers.Conv2D(32, (3, 3), activation='relu'),
 #     layers.MaxPooling2D((2, 2)),
-#     layers.Conv2D(64, (3, 3), activation='relu'),
+#     layers.Conv2D(32, (3, 3), activation='relu'),
 #     layers.MaxPooling2D((2, 2)),
 #     layers.Flatten(),
 #     layers.Dense(128, activation='relu'),
 #     layers.Dropout(0.5),
 #     layers.Dense(4, activation='softmax')
 # ])
-
 model = models.Sequential([
     layers.Input(shape=(X_train.shape[1], X_train.shape[2], 1)),
+
     layers.Conv2D(32, (3, 3), activation='relu'),
     layers.MaxPooling2D((2, 2)),
+    layers.BatchNormalization(),
+
     layers.Conv2D(32, (3, 3), activation='relu'),
     layers.MaxPooling2D((2, 2)),
+    layers.BatchNormalization(),
+
     layers.Flatten(),
+
     layers.Dense(128, activation='relu'),
     layers.Dropout(0.5),
+
     layers.Dense(4, activation='softmax')
 ])
+
 
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
