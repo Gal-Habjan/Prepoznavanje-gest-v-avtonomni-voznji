93cb715f5105fddddaa17129b8d4bad84937c042
crazycake919 <galhabjan100@gmail.com>
2024-06-13 22:36:00 +0200

    Added ai voice
    GES-28
    dodal voice AI
    dodal povezavo do spotify
    izpis funkcionalnosti

--- main_projekt/UI.py
+++ main_projekt/UI.py
@@ -5,10 +5,16 @@ from enum import IntEnum
 import spotifyApp
 import webbrowser
 from threading import Thread
+import threading
 import time
 import shell_image
 import numpy as np
+from datetime import datetime
+import paho.mqtt.client as mqtt
 from prometheus_client import start_http_server, Counter
+import voice_recognition as voice
+
+
 class Gestures(IntEnum):
     FIST = 0
     HAND = 1
@@ -44,24 +50,46 @@ class CameraApp:
         self.video_source = 0
         self.vid = cv2.VideoCapture(self.video_source)
 
+
         self.canvas = tk.Canvas(master, width=self.vid.get(cv2.CAP_PROP_FRAME_WIDTH),
                                 height=self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
         self.canvas.pack()
-        self.turn_down_volume_counter = Counter('volume_down', 'volume down counter')
-        self.turn_up_volume_counter = Counter('volume_up', 'volume up counter')
-        self.toggle_pause_counter = Counter('toggle_pause', 'toggle pause counter')
-        self.next_song_counter = Counter('next_song', 'next song counter')
         self.gesture_label = tk.Label(master, text="Waiting..")
         self.gesture_label.pack(anchor=tk.CENTER, expand=True)
+        # self.turn_down_volume_counter = Counter('volume_down', 'volume down counter')
+        # self.turn_up_volume_counter = Counter('volume_up', 'volume up counter')
+        # self.toggle_pause_counter = Counter('toggle_pause', 'toggle pause counter')
+        # self.next_song_counter = Counter('next_song', 'next song counter')
+        # self.send_requests = Counter('all_requests', 'all processed images and sent requests')
+        # self.broker = "10.8.11.3"
+        #
+        #
+        # self.port = 1883
+        # self.topic = "/data"
+        # self.mqtt_client = mqtt.Client(client_id="producer_1", callback_api_version=mqtt.CallbackAPIVersion.VERSION2)
+        # self.mqtt_client.connect(self.broker, self.port, 360)
+        # self.mqtt_client.on_connect = self.on_connect
+        # start_http_server(8000)
         self.vid = cv2.VideoCapture(0)
         self.snapshot_queue = []
         self.current_box = None
         self.current_prob = None
+        self.lock = threading.Lock()
         self.update()
+
         button = tk.Button(master, text="Authenticate", command=self.open_url)
         button.pack(pady=20)
+        self.voice_label = tk.Label(master, text="Click listen and speak for 3 seconds")
+        self.voice_label.pack(anchor=tk.CENTER, expand=True)
+        button2 = tk.Button(master, text="Listen", command=self.listen_to_voice_button)
+        button2.pack(pady=10)
         self.spotify_thread = None
-        start_http_server(8000)
+        self.voice_thread = None
+
+        # self.wait_for_sound()
+
+    def on_connect(self, client, userdata, flags, reasonCode, properties=None):
+        print("Povezava z GESTA MQTT: " + str(reasonCode))
 
     def open_url(self):
         url = "http://127.0.0.1:5000"  # Replace this with your desired URL
@@ -73,6 +101,11 @@ class CameraApp:
 
     def close_window(self):
         self.is_running = False
+
+        if self.spotify_thread and self.spotify_thread.is_alive():
+            self.spotify_thread.join()
+        if self.voice_thread and self.voice_thread.is_alive():
+            self.voice_thread.join()
         self.master.destroy()
 
     def snapshot(self):
@@ -83,34 +116,70 @@ class CameraApp:
             # Save the image using PIL
             return pil_image
 
-    def call_spotify(self, gesture):
+    def call_spotify(self, gesture, function=None):
+
         try:
-            function = gestures_features_dict[gesture]
+            # message = "sending gesture" + gesture.name+ " " + datetime.now().strftime("%H:%M:%S")
+            # ret = self.mqtt_client.publish(self.topic, message, qos=1, retain=False)
+            # self.send_requests.inc(1)
+            # print("Pošiljanje: " + message + " " + str(ret.rc))
+            if gesture is not None:
+                function = gestures_features_dict[gesture]
+
             if function == Features.NEXT_SONG:
-                self.next_song_counter.inc(1)
+                # self.next_song_counter.inc(1)
                 spotifyApp.call_next_song()
 
             elif function == Features.TOGGLE_PAUSE:
-                self.toggle_pause_counter.inc(1)
+                # self.toggle_pause_counter.inc(1)
                 spotifyApp.call_toggle_pause()
 
             elif function == Features.TURN_UP_VOL:
-                self.turn_up_volume_counter.inc(1)
+                # self.turn_up_volume_counter.inc(1)
                 spotifyApp.call_volume_up()
 
             elif function == Features.TURN_DOWN_VOL:
-                self.turn_down_volume_counter.inc(1)
+                # self.turn_down_volume_counter.inc(1)
                 spotifyApp.call_volume_down()
         except Exception as e:
             print("Exception while calling spotify", e)
         time.sleep(1)
 
-    def draw_hull_on_image(self,original_image, hull, offset=(0, 0)):
+    def listen_to_voice_button(self):
+        if self.voice_thread is None or not self.voice_thread.is_alive():
+            print("start thread")
+            self.voice_thread = Thread(target=self.wait_for_sound)
+
+            self.voice_thread.start()
+
+    def wait_for_sound(self):
+        with self.lock:
+            print("Start listening")
+            try:
+                predicted_command, probabilities = voice.get_command_from_voice()
+                print(f"Predicted Command: {predicted_command}")
+                feature = Features.DO_NOTHING
+                if predicted_command == Features.TURN_UP_VOL.name:
+
+                    feature = Features.TURN_UP_VOL
+                elif predicted_command == Features.TURN_DOWN_VOL.name:
+                    feature = Features.TURN_DOWN_VOL
+                elif predicted_command == Features.NEXT_SONG.name:
+                    feature = Features.NEXT_SONG
+
+                elif predicted_command == Features.TOGGLE_PAUSE.name:
+                    feature = Features.TOGGLE_PAUSE
+
+                self.voice_label.config(text=feature.name)
+                self.call_spotify(None, feature)
+            except Exception as e:
+                print(f"Error in wait_for_sound: {e}")
+
+    def draw_hull_on_image(self, original_image, hull, offset=(0, 0)):
         if hull is None:
             return original_image
 
         points = hull.points[hull.vertices]
-
 
         # ok flippam x pa y ker nevem zakaj obrne okol
         points = points[:, ::-1]
@@ -118,6 +187,7 @@ class CameraApp:
         points += np.array(offset)  # Apply the offset to get back to the original image coordinates
         cv2.polylines(original_image, [points], isClosed=True, color=(0, 255, 0), thickness=2)
         return original_image
+
     def update(self):
         ret, frame = self.vid.read()
         if ret:
@@ -139,7 +209,6 @@ class CameraApp:
                     rgb_frame = self.draw_hull_on_image(rgb_frame, hull, offset)
                 except Exception as e:
                     print("Exception while getting hull", e)
-
 
                 gesture = Gestures.NONE
                 if clas == Gestures.FIST.value:
@@ -173,5 +242,3 @@ class CameraApp:
     def __del__(self):
         if self.vid.isOpened():
             self.vid.release()
-
-# Create a master and pass it to the CameraApp class
