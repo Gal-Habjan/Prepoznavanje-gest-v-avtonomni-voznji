5c07a76a1f3ee7e918827374fb6a19d92ac3ba47
TheWanderer321 <jasa.marolt@gmail.com>
2025-01-06 16:54:35 +0100

    added camera

--- 3d frontend/main.js
+++ 3d frontend/main.js
@@ -1,8 +1,13 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { FBXLoader } from "three/addons/loaders/FBXLoader.js"; // Use FBXLoader instead of OBJLoader
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+// const gltfloader = new GLTFLoader();
+const loader = new FBXLoader(); // Use FBXLoader to load FBX models
+
 
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xffffff);
 const camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
@@ -13,9 +18,8 @@ const camera = new THREE.PerspectiveCamera(
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-
 const controls = new OrbitControls(camera, renderer.domElement);
-const loader = new FBXLoader(); // Use FBXLoader to load FBX models
+
 const textureLoader = new THREE.TextureLoader();
 const texture = textureLoader.load("./bake-2.png"); // Replace with your texture path
 let mixer;
@@ -30,6 +34,7 @@ loader.load(
         });
         object.position.set(0, 0, 0); // Adjust position if needed
         object.scale.set(0.05, 0.05, 0.05);
+        object.position.set(10,1,1)
         scene.add(object);
         mixer = new THREE.AnimationMixer(object);
 
@@ -53,12 +58,39 @@ loader.load(
         console.error("An error occurred while loading the FBX file:", error);
     }
 );
+const cameraTexture = textureLoader.load("./esp32camBake.PNG");
+loader.load(
+  "./camera.fbx", // Path to your FBX file
+  (object) => {
+      object.traverse((child) => {
+          if (child.isMesh) {
+              child.material = new THREE.MeshBasicMaterial({ map: cameraTexture }); // Apply texture
+          }
+      });
+      object.position.set(-5, 0, 0); // Adjust position if needed
+      object.scale.set(0.05, 0.05, 0.05);
+      scene.add(object);
 
+
+
+  },
+  (xhr) => {
+      console.log(`Loading: ${(xhr.loaded / xhr.total) * 100}% complete`);
+  },
+  (error) => {
+      console.error("An error occurred while loading the FBX file:", error);
+  }
+);
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
+cube.position.set(0,0,0)
 scene.add(cube);
 
+
+
+
+ 
 camera.position.z = 5;
 const socket = new WebSocket("ws://localhost:8765");
 
